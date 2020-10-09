605659d55bd56c59b25d22dfd02fe356cce8ecb6
ybZhang <472099249@qq.com>
2020-10-09 21:21:13 +0800

    deploy

---
+++ precache-manifest.423c06772bebf7dcd5af503ab81cf7c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5169234e8beeaa4637c3b10951593a3a",
+    "url": "/book-keeping-react-website/index.html"
+  },
+  {
+    "revision": "dd853be88cee64585d90",
+    "url": "/book-keeping-react-website/static/css/main.817cd69c.chunk.css"
+  },
+  {
+    "revision": "94ad9eb07368294fadb9",
+    "url": "/book-keeping-react-website/static/js/2.6174081d.chunk.js"
+  },
+  {
+    "revision": "6f8c325a33ab740f7f910ec9ee534ac9",
+    "url": "/book-keeping-react-website/static/js/2.6174081d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd853be88cee64585d90",
+    "url": "/book-keeping-react-website/static/js/main.675db0fe.chunk.js"
+  },
+  {
+    "revision": "a93a317c4bc1480a7c49",
+    "url": "/book-keeping-react-website/static/js/runtime-main.3b203c5d.js"
+  }
+]);
